11ad00f8724af31f18248a72523dda538003365c
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2020-04-26 14:08:23 +0300

    eka versio

---
+++ public/js/leffat.js
@@ -0,0 +1,14 @@
+
+async function hae_elokuva(){
+  var leffan_nimi = document.getElementById("leffa").value;
+  var vuosi = document.getElementById("vuosi").value;
+  var posteri = document.getElementById("leffan_posteri");
+
+  var hakurimpsu = 'http://www.omdbapi.com/?t=' + leffan_nimi + '&y=' + vuosi +'&apikey=a95f3723';
+  console.log(hakurimpsu);
+  const response = await fetch(hakurimpsu);
+  const data = await response.json();
+  console.log(data);
+  document.getElementById("elokuvan_nimi").innerHTML = data.Title;
+  posteri.src = data.Poster;
+}
---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express();
+//app.listen(3000, () => console.log("kuuntelen"));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("kuuntelen porttia " + port));
+app.use(express.static("public"));
